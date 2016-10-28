56612d083f0cfb45a47a583b8e60e1534842c5af
nikraina <raina.nith@gmail.com>
2016-10-28 14:22:38 -0400

    noun counter

--- extract_noun.py
+++ extract_noun.py
@@ -5,16 +5,13 @@ from collections import Counter
 
 fileDir = os.path.dirname(os.path.realpath('__file__'))
 filename = fileDir + '/articles/12th_Street_Riot.txt'
-# filename = os.path.abspath(os.path.realpath(filename))
 print(fileDir)
 print(filename)
 
 
-# file = open('/articles/12th_Street_Riot.txt')
 file = open(filename, mode='r')
 lines = file.read()
 
-# lines = 'lines is some string of words'
 
 is_noun = lambda pos: pos[:2] == 'NN'
 
