e5f7ea2c7116b9b8f484b1b2629c4963efefebdb
nikign <niki.hp2007@gmail.com>
2016-11-25 15:18:29 -0500

    get articles for africa riots

--- seach_bing.py
+++ seach_bing.py
@@ -4,17 +4,19 @@ from py_bing_search import PyBingWebSearch
 import urllib2, time, os, shutil, requests
 
 api_key = 'OP14ieri+Elhn7fChsHgYTJKCnasjdy783spq7QVFlM'
+riots_file = 'africa_list_of_riots.txt'
 
 def write_text():
-	with open("listofriots.txt") as listofriots:
+	with open(riots_file) as listofriots:
 		for i, l in enumerate(listofriots):
-			if i < 171: # to pass the ones we already have
+			if i < 0: # to pass the ones we already have
 				continue
 			try:
 				print "looking at riot %d" %i
-				name  = l.split("-")[1].strip().split('(')[0].strip()
+				name  = l.strip()
+				# name  = l.split("-")[1].strip().split('(')[0].strip()
 				bing_news = PyBingWebSearch(api_key, name)
-				news = bing_news.search(limit=30, format='json')
+				news = bing_news.search(limit=5, format='json')
 				time.sleep(3)
 				try:
 				    shutil.rmtree("riots_bing/riot_%02d" % i)
@@ -29,27 +31,28 @@ def write_text():
 				continue
 
 def write_html():
-	with open("listofriots.txt") as listofriots:
+	with open(riots_file) as listofriots:
 		for i, l in enumerate(listofriots):
 			if i < 0:
 				continue
 			try:
 				print "looking at riot %d" %i
-				name  = l.split("-")[1].strip().split('(')[0].strip()
+				name  = l.strip()
+				# name  = l.split("-")[1].strip().split('(')[0].strip()
 				bing_news = PyBingNewsSearch(api_key, name)
-				news = bing_news.search(limit=30, format='json')
+				news = bing_news.search(limit=5, format='json')
 				time.sleep(3)
 				try:
-				    shutil.rmtree("riots_bing_html/riot_%02d" % i)
+				    shutil.rmtree("riots_africa/riot_%04d" % i)
 				except OSError:
 				    pass
-				os.mkdir("riots_bing_html/riot_%02d" % i)
+				os.mkdir("riots_africa/riot_%04d" % i)
 				for j, new in enumerate(news):
-					with open('riots_bing_html/riot_%02d/%02d.html' %(i, j), 'w') as riot_file:
+					with open('riots_africa/riot_%04d/%d.html' %(i, j), 'w') as riot_file:
 						web = requests.get(new.url)
 						riot_file.write(web.text.encode('utf-8').strip())
 						# riot_file.write(new.description.encode('utf-8').strip())
 			except:
 				continue
 
-write_text()
+write_html()
