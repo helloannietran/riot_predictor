d8f96ba684dba2758f6815bbe83d2ede76b477f3
nikraina <raina.nith@gmail.com>
2016-11-28 08:53:09 -0500

    make structures for articles

--- web_app/app/views.py
+++ web_app/app/views.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask import render_template
 from app import app
-# from app.twitter.views import twitter
+from app.twitter.views import twitter
 
 # app = Flask(__name__)
-# app.register_blueprint(twitter, url_prefix='/twitter')
+app.register_blueprint(twitter, url_prefix='/twitter')
 
 
 @app.route('/')
